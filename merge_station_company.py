85ca321265a1cdb1cc10aa685dd46cf2714b6b40
jason-sa <jandlsalazeradams@gmail.com>
2018-09-27 08:30:09 -0700

    added mean/max calculations

--- merge_station_company.py
+++ merge_station_company.py
@@ -2,7 +2,7 @@ import numpy as np
 from geopy.distance import geodesic
 import pandas as pd
 
-def merge_station_company(stations, companies):
+def merge_station_company(stations, companies, turnstiles):
     
     # cross join the two data frames
     stations['_tmpkey'] = 1
@@ -13,11 +13,30 @@ def merge_station_company(stations, companies):
     station_companies.name = station_companies.name.str.replace('rd ',' ')
     station_companies.name = station_companies.name.str.replace('th ',' ')
     station_companies.name = station_companies.name.str.replace('st ',' ')
+    station_companies.name = station_companies.name.str.replace('1 Ave', '1 AV')
     
+    # set station as the index for joining
+    station_companies.rename(columns={'name':'STATION'}, inplace=True)
+    station_companies.STATION = station_companies.STATION.str.upper()
+    station_companies.index = station_companies.STATION
+    station_companies.rename(columns={'STATION':'STAION_nm'}, inplace=True)
+    
+    # calculate distance
     station_companies['distance'] = np.nan
     station_companies['comp_lat_lon'] = list(zip(station_companies.LAT, station_companies.LON))
     station_companies['station_lat_lon'] = list(zip(station_companies.lat, station_companies.lon))
     station_companies['distance'] = [geodesic(v, station_companies.iloc[k,10]).miles for k, v in enumerate(station_companies.comp_lat_lon)]
+    
+    # calculate mean / max stats by station
+    station_mean = turnstiles.groupby('STATION')['hourly_entries'].mean()
+    station_max = turnstiles.groupby('STATION')['hourly_entries'].max()
+
+    # be aware of data errors -- need to determine if further cleaning is needed. (2996/6622)
+    station_companies = station_companies.join(station_mean, how='inner')
+    station_companies.rename(columns={'hourly_entries':'mean_entries'}, inplace=True)
+
+    station_companies = station_companies.join(station_max, how='inner')
+    station_companies.rename(columns={'hourly_entries':'max_entries'}, inplace=True)
     
 
     return station_companies
