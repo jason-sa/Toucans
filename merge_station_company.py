336ae1e1f2f6244ad5bc3ed7db6fa0276a8789fd
jason-sa <jandlsalazeradams@gmail.com>
2018-09-27 13:10:08 -0700

    aggreagated by station

--- merge_station_company.py
+++ merge_station_company.py
@@ -3,11 +3,13 @@ from geopy.distance import geodesic
 import pandas as pd
 
 def merge_station_company(stations, companies, turnstiles):
-    
+    # agg to station level taking the min lat/long
+    station_agg = stations.groupby('name').min().reset_index()[['name','lon','lat']]
+
     # cross join the two data frames
-    stations['_tmpkey'] = 1
+    station_agg['_tmpkey'] = 1
     companies['_tmpkey'] = 1
-    station_companies = pd.merge(companies, stations, how='outer', on='_tmpkey').drop('_tmpkey', axis=1)
+    station_companies = pd.merge(companies, station_agg, how='outer', on='_tmpkey').drop('_tmpkey', axis=1)
     
     # remove rd, th, and st to match stations with the turnstile data
     station_companies.name = station_companies.name.str.replace('rd ',' ')
@@ -25,7 +27,7 @@ def merge_station_company(stations, companies, turnstiles):
     station_companies['distance'] = np.nan
     station_companies['comp_lat_lon'] = list(zip(station_companies.LAT, station_companies.LON))
     station_companies['station_lat_lon'] = list(zip(station_companies.lat, station_companies.lon))
-    station_companies['distance'] = [geodesic(v, station_companies.iloc[k,10]).miles for k, v in enumerate(station_companies.comp_lat_lon)]
+    station_companies['distance'] = [geodesic(v, station_companies.iloc[k,8]).miles for k, v in enumerate(station_companies.comp_lat_lon)]
     
     # calculate mean / max stats by station
     station_hour_day = turnstiles.groupby(['STATION','DATE','hour'])['hourly_entries'].sum()
